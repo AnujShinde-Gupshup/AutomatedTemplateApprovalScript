5bfb7b6eda6fc1cfcdae76f3b8265f7c2417b48e
Anuj <anuj@bom2ad.gupshup.io>
2021-05-27 22:34:51 +0530

    firstpush

---
+++ index.js
@@ -0,0 +1,12 @@
+var templateList = require('./Template-Applist.json')
+const fs = require('fs')
+const app = require('express')();
+const port =
+
+app.listen(8080, ()=> {
+    console.log("listening on port 8080")
+})
+
+var date = Date.now()
+
+console.log(date)
